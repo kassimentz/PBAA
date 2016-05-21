d31838b0a7d92522e5b1cc48900aaf3b80fb83eb
KASSIANE SILVA MENTZ <630910144@SALA309PC16.local>
2016-05-21 15:04:56 -0300

    Projeto Fragmments

---
+++ Aula04/Fragments/app/src/main/java/edm/kassimentz/fragments/MainActivity.java
@@ -0,0 +1,26 @@
+package edm.kassimentz.fragments;
+
+
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void novoFragment(View v){
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        SegundaFragment segundaFragment = new SegundaFragment();
+        fragmentTransaction.replace(android.R.id.content, segundaFragment);
+        fragmentTransaction.commit();
+    }
+}
